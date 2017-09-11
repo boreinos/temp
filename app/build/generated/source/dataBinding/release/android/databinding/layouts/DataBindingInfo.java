dd9fe272abab89cbe6da142a22b5ade88ff437de
DHatch <dhatch@afilon.com>
2017-09-11 08:36:48 -0400

    todos to flag vote

---
+++ app/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="df5206d0-d5ca-4fe5-94d2-e25676258c4d")
+public class DataBindingInfo {}
