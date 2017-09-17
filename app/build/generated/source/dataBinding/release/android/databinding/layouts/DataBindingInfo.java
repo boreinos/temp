cea9e4430fe6d38bbd694d72ac30c7c6e73afa9e
Boris Reinosa <breinosa@afilon.com>
2017-09-16 21:53:11 -0400

    .gitignore update

--- app/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="cebb707e-82c3-4453-acf5-cfbe72a38646")
-public class DataBindingInfo {}
