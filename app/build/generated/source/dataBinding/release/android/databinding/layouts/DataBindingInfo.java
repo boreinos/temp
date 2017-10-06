10b3f2938d2d4b812ba3223ad87df21b4147f9a2
Carlos Cano <ccano@afilon.com>
2017-10-06 13:13:43 -0400

    voting_center, preferential_election_candidates and preferential_election_parties updated. Also cantidate pictures where udpated

--- app/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="4c761d16-94c4-4041-8c50-ffbc550432b1")
+@BindingBuildInfo(buildId="453fc058-e2ca-4820-a25e-39dca570b623")
 public class DataBindingInfo {}
